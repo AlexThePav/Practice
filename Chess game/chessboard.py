48f8db4abf3a954b336b21735fa864f66dda48ad
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2018-11-04 23:32:17 +0200

    image finally

--- Chess game/chessboard.py
+++ Chess game/chessboard.py
@@ -5,27 +5,26 @@ try:
 except ImportError:
     import Tkinter as tkinter
 
-from PIL import ImageTk, Image
-
-
-def load_images():
-    name = 'Background/OOAD630.jpg'
-    tkinter.PhotoImage(Image.open(file=name))
-
 
 coordinates = [[1, 2, 3, 4, 5, 6, 7, 8], ["a", "b", "c", "d", "e", "f", "g", "h"]]
 
-mainWindowPadding = 10
-
+# Main window
+mainWindowPadding = 0
 mainWindow = tkinter.Tk()
 mainWindow.title("Chessboard")
-mainWindow.geometry("640x640")
+mainWindow.geometry("1164x1164")
 mainWindow['padx'] = mainWindow['pady'] = mainWindowPadding
 
-# chess pad to hold background image
-chessPad = tkinter.Frame(mainWindow)
-chessPad.grid(row=0, column=0, sticky="nsew")
-load_images()
+# Frame to hold background image
+background = tkinter.Frame(mainWindow)
+background.grid(row=0, column=0, sticky="nsew", padx=0, pady=0)
+
+# Load Background
+imgPath = 'Background/OOAD630.png'
+chessboardImage = tkinter.PhotoImage(file=imgPath)
+imgLabel = tkinter.Label(image=chessboardImage)
+# imgLabel.image = chessboardImage
+imgLabel.grid(row=0, column=0, sticky="nsew", padx=0, pady=0)
 
 # row = 0
 # for sqRow in coordinates[0]:
